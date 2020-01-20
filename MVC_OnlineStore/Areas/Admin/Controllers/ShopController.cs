5fe134ac47ebc0d1ac2dcae1044d2dc8b97c620f
arseniy <arstrybuh@gmail.com>
2020-01-20 12:58:35 +0200

    Add, Sort, Delete  for Categories was implement

--- MVC_OnlineStore/Areas/Admin/Controllers/ShopController.cs
+++ MVC_OnlineStore/Areas/Admin/Controllers/ShopController.cs
@@ -1,4 +1,6 @@
-﻿using MVC_OnlineStore.DAL;
+﻿using MVC_OnlineStore.Areas.Admin.Infrastructure;
+using MVC_OnlineStore.DAL;
+using MVC_OnlineStore.Models.DataModels;
 using MVC_OnlineStore.Models.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -19,6 +21,63 @@ namespace MVC_OnlineStore.Areas.Admin.Controllers
                 
 
             return View(categories);
+        }
+
+        //Post: Admin/Shop/AddCategory
+        [HttpPost]
+        public string AddCategory(string catName)
+        {
+            string id;
+
+            if(db.Categories.Any(x=>x.Name == catName))
+            {
+                return "titletaken";
+            }
+            Category model = new Category();
+            model.Name = catName;
+            model.Description = catName.Replace(' ', '-').ToLower();
+            model.Sorting = 100;
+            db.Categories.Add(model);
+            db.SaveChanges();
+
+            id = model.Id.ToString();
+
+            return id;
+        }
+
+        // GET: Admin/Shop/DeleteCategory
+        [HttpGet]
+        public ActionResult DeleteCategory(int id)
+        {
+            Category category = db.Categories.Find(id);
+
+            if (category == null)
+            {
+                return Content("Страница не найдена.");
+            }
+
+            db.Categories.Remove(category);
+            db.SaveChanges();
+
+            TempData["Message"] = "Категория была успешно удалена.";
+            return RedirectToAction("Index");
+        }
+
+        //Post: Admin/Shop/ReorderCategories
+        public void ReorderCategories(ReorderHelper rh)
+        {
+            int[] ids = rh.id.ToArray();
+            int count = 1;
+            Category category;
+            foreach (var categoryId in ids)
+            {
+                category = db.Categories.Find(categoryId);
+                category.Sorting = count;
+
+                db.SaveChanges();
+
+                count++;
+            }
         }
     }
 }
