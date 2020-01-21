024fcb3d1984e6315e2b719cc1cb0e2b41273ac2
arseniy <arstrybuh@gmail.com>
2020-01-21 15:21:37 +0200

    scripts have received into external files

---
+++ MVC_OnlineStore/Content/scripts/pages.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+
+    $("a.delete").click(function () {
+        if (!confirm("Подтвердите удаление страницы.")) return false;
+    });
+
+    $("table#pages tbody").sortable({
+        items: "tr:not(.home)",
+        placeholder: "ui-state-highlight",
+        update: function () {
+            var ids = $("table#pages tbody").sortable("serialize");
+            var url = "/Admin/Pages/ReorderPages";
+            $.post(url, ids);
+        }
+    });
+});
